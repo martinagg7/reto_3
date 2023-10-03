505408cf9987c557064a118dfa2ff9fcd2332393
Martina Garcia <martinagg@myuax.com>
2023-10-03 09:05:58 +0200

    .

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+#queremos encontrar todos los números distintos que hay en una lista  sin emplear sets, listas , etc
+
+asistencia=int(input("Asistentes en la sala:"))
+l=list(map(int,input().split()))
+
+def pasar_lista():
+    asistentes=0
+    l.sort()
+    for i in range(len(l)-1):
+        if l[i]!=l[i+1]:
+            asistentes+=1
+    asistentes+=1#tenemos que sumar siempre uno del primer elemento
+    return asistentes
